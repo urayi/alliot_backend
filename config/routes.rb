1814a9b63f8ebbb6c40322f3663780dedab7182c
Jose Polanco <urayi@URAYI-NITRO5.localdomain>
2020-08-01 14:28:20 -0400

    Configurado Registro, Login, Logout de usuario

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  # Authentication
+  devise_for :users, skip: %i[registrations sessions passwords]
+  devise_scope :user do
+    post '/signup', to: 'registrations#create'
+    post  '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  # test "should get Registrations" do
+  #   get sessions_Registrations_url
+  #   assert_response :success
+  # end
+  # test "the truth" do
+  #   assert true
+  # end
+end
