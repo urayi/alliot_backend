50c24e2ccd94a4113b034d4c334acec71562bd16
Jose Polanco <urayi@URAYI-NITRO5.localdomain>
2020-08-01 22:23:31 -0400

    Definiendo recursos

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,12 +311,12 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.jwt_secret
-  jwt.dispatch_requests = [
-    ['POST', %r{^/login$}]
-  ]
-  jwt.revocation_requests = [
-    ['DELETE', %r{^/logout$}]
-  ]
-  jwt.expiration_time = 1.day.to_i
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rails.application.routes.draw do
 
-  # Authentication
+  # Registro y Autenticación de usuario
   devise_for :users, skip: %i[registrations sessions passwords]
   devise_scope :user do
     post '/signup', to: 'registrations#create'
     post  '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
   end
+
+  # Usuarios
+  resources :users
+
+  # Requerimientos
+  # resources :requeriments
+
+  # Comentarios
+  # resources :comments
+
+  # Votos
+  # resources :votes
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
