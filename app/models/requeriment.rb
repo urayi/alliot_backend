c5b13d77c665349fbc7b2a291414360b655aca5c
Jose Polanco <urayi@URAYI-NITRO5.localdomain>
2020-08-03 01:08:53 -0400

    Se validan que existan los parametros de entrada

--- app/models/requeriment.rb
+++ app/models/requeriment.rb
@@ -1,6 +1,6 @@
 class Requeriment < ApplicationRecord
-  validates :title, :presence => true
-  validates :content, :presence => true
+  validates :title, :presence => true, on: :create
+  validates :content, :presence => true, on: :create
   belongs_to :user, foreign_key: "user_id"
   has_many :comments, foreign_key: "requeriment_id"
   has_many :votes, foreign_key: "requeriment_id"
